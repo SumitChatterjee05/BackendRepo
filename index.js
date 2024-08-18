bfca529fa2030a5062b3c2a3385d255ed740d31f
Sumit Chatterjee <sumitchatterjee583@gmail.com>
2024-08-18 08:18:11 +0530

    #dontaddthiscommit

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 import mongoose from "mongoose";
 import connectDB from "./src/DB/database.js";
 import dotenv from "dotenv";
+import { app } from "./src/app.js";
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(()=>
+    {
+        app.listen(process.env.PORT || 8000), () =>{
+        {
+            console.log("SERVER IS RUNNING :)");
+        }
+    }})
+.catch
+(
+    (error) =>
+    {
+        console.log("MONGO DB ERROR FAIL!!");
+    }
+)
